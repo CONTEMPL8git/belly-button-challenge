d59b23500c00513899a375b738882886e6fd21b2
CONTEMPL8git <christopherloch@hotmail.com>
2024-04-30 17:06:46 -0500

    bar chart added

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,16 +1,27 @@
-const filepath = "./samples.json";
+const filepath = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
 d3.json(filepath).then(function(data) {
-    console.log(data);
+   console.log("data", data);
+    console.log("data.samples", data.samples)
+    console.log("data.samples[0]", data.samples[0].id)
     populateSubjectID(data.names);
 
+    var data_2 = [
+        {
+          x: ['giraffes', 'orangutans', 'monkeys'],
+          y: [20, 14, 23],
+          type: 'bar'
+        }
+      ];
+      
+      Plotly.newPlot('bar', data_2);
     
   });
 
 
 
 function populateSubjectID(names){
-    console.log(names);
+  //  console.log(names);
     d3.select("select")
     .selectAll("option")
     .data(names)
@@ -25,5 +36,5 @@ function populateSubjectID(names){
 
 function changeSubjectId(subjectId){
    
-    console.log(subjectId);
+  //  console.log(subjectId);
 };
